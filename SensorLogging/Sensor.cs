77802266d1ae7aa5aa55b64940ceb5dd8299ce48
Andrew Hayden <contact@andrewhayden.dev>
2024-05-08 01:09:39 -0400

    quick lil sensor logger api

---
+++ SensorLogging/Sensor.cs
@@ -0,0 +1,9 @@
+﻿namespace SensorLogging
+{
+    public class Sensor
+    {
+        public int ID { get; set; } = -1;
+        public string Message { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
